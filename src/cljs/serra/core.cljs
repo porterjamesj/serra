200a75b7c6893c6b2d28bfe8cf1db313edc82c5f
James Porter <porterjamesj@gmail.com>
2014-09-27 22:42:49 -0700

    more wip

--- src/cljs/serra/core.cljs
+++ src/cljs/serra/core.cljs
@@ -1,7 +1,9 @@
 (ns serra.core
-    (:require [serra.dev :refer [is-dev?]]
-              [om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require [serra.dev :refer [is-dev?]]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
 
 (def app-state (atom {:players [{:name "James" :life 30}
                                 {:name "Rachel" :life 40}]}))
@@ -16,20 +18,33 @@
       (apply dom/ul nil
         (om/build-all player-view
           (vec (map (fn [p] {:player p
-                             :max-life (apply max (map :life players))})
+                             :max-life (max 20 (apply max (map :life players)))})
                     players)))))))
 
 (defn player-view [{:keys [player max-life]} owner]
   (reify
-    om/IRender
-    (render [_]
+    om/IInitState
+    (init-state [_]
+      {:life-updates (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [updates (om/get-state owner :life-updates)]
+        (go-loop []
+          (let [n (<! updates)]
+            (om/update! player :life n)
+            (recur)))))
+    om/IRenderState
+    (render-state [_ {:keys [life-updates]}]
       (dom/div {:className "player"}
         (dom/h2 nil (:name player))
-        (dom/div nil (:life player))
-        (dom/input #js {:type "range"
-                        :value (:life player)
-                        :max max-life
-                        :onChange notify})))))
+        (dom/input #js {:type "text" :value (:life player)
+                        :onKeyUp #(put! life-updates (.. % -target -value))})
+        (dom/progress #js {:value (:life player)
+                           :max max-life})
+        (dom/button #js {:onClick
+                         (fn [e] (put! life-updates (dec (:life @player))))} "-")
+        (dom/button #js {:onClick
+                         (fn [e] (put! life-updates (inc (:life @player))))} "+")))))
 
 (om/root
  (fn [{:keys [players] :as app} owner]
