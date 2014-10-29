4ad7da2e9557563b60d53096d328a82267ee149f
James Porter <porterjamesj@gmail.com>
2014-10-28 19:37:42 -0500

    upgrade chestnut stuff

--- README.md
+++ README.md
@@ -14,15 +14,40 @@ In the REPL do
 (browser-repl)
 ```
 
-The first starts the webserver at 10555. The second starts the Weasel
-REPL server.
+The call to `(run)` does two things, it starts the webserver at port
+10555, and also the Figwheel server which takes care of live reloading
+ClojureScript code and CSS. Give them some time to start.
 
-In a terminal do `lein figwheel`, this will watch and recompile your
-ClojureScript, and start the figwheel server (the
-default). Whenever your code changes, figwheel will recompile it and
-send it to the browser immediately.
+Running `(browser-repl)` starts the Weasel REPL server, and drops you
+into a ClojureScript REPL. Evaluating expressions here will only work
+once you've loaded the page, so the browser can connect to Weasel.
 
-Now browse to `http://localhost:10555` and enjoy.
+When you see the line `Successfully compiled "resources/public/app.js"
+in 21.36 seconds.`, you're ready to go. Browse to
+`http://localhost:10555` and enjoy.
+
+**Attention: It is not longer needed to run `lein fighwheel`
+  separately. This is now taken care of behind the scenes**
+
+## Trying it out
+
+If all is well you now have a browser window saying 'Hello Chestnut',
+and a REPL prompt that looks like `cljs.user=>`.
+
+Open `resources/public/css/style.css` and change some styling of the
+H1 element. Notice how it's updated instantly in the browser.
+
+Open `src/cljs/chestnut/core.cljs`, and change `dom/h1` to
+`dom/h2`. As soon as you save the file, your browser is updated.
+
+In the REPL, type
+
+```
+(ns chestnut.core)
+(swap! app-state assoc :text "Interactivity FTW")
+```
+
+Notice again how the browser updates.
 
 ## Deploying to Heroku
 
--- project.clj
+++ project.clj
@@ -7,36 +7,32 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2342"]
+                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
                  [ring "1.3.1"]
-                 [compojure "1.1.9"]
+                 [compojure "1.2.0"]
                  [enlive "1.1.5"]
-                 [om "0.7.1"]
+                 [om "0.7.3"]
                  [figwheel "0.1.4-SNAPSHOT"]
                  [environ "1.0.0"]
                  [com.cemerick/piggieback "0.1.3"]
-                 [weasel "0.4.0-SNAPSHOT"]]
+                 [weasel "0.4.0-SNAPSHOT"]
+                 [leiningen "2.5.0"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
 
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.0"
 
   :uberjar-name "serra.jar"
 
   :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
-                             :compiler {:output-to     "resources/public/app.js"
-                                        :output-dir    "resources/public/out"
-                                        :source-map    "resources/public/out.js.map"
+                             :compiler {:output-to     "resources/public/js/app.js"
+                                        :output-dir    "resources/public/js/out"
+                                        :source-map    "resources/public/js/out.js.map"
                                         :preamble      ["react/react.min.js"]
                                         :externs       ["react/externs/react.js"]
                                         :optimizations :none
-                                        :pretty-print  true}}
-                       :prod {:source-paths ["src/cljs"]
-                              :compiler {:output-to     "target/app.js"
-                                         :preamble      ["react/react.min.js"]
-                                         :externs       ["react/externs/react.js"]
-                                         :optimizations :advanced}}}}
+                                        :pretty-print  true}}}}
 
   :profiles {:dev {:repl-options {:init-ns serra.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
@@ -44,13 +40,15 @@
                    :figwheel {:http-server-root "public"
                               :port 3449
                               :css-dirs ["resources/public/css"]}
-                   :env {:is-dev true}}
+                   :env {:is-dev true}
+                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]}}}}
 
              :uberjar {:hooks [leiningen.cljsbuild]
                        :env {:production true}
                        :omit-source true
                        :aot :all
                        :cljsbuild {:builds {:app
-                                            {:compiler
+                                            {:source-paths ["env/prod/cljs"]
+                                             :compiler
                                              {:optimizations :advanced
                                               :pretty-print false}}}}}})
